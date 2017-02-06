5eb378d1a47287554566d1b767e2adb5ed8f2fc5
kdevkr <kdevkr@gmail.com>
2017-02-06 09:38:48 +0900

    init

--- src/main/java/com/kdev/app/HomeController.java
+++ src/main/java/com/kdev/app/HomeController.java
@@ -1,8 +1,12 @@
 package com.kdev.app;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -34,6 +39,24 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
+	public @ResponseBody Object json(){
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Spring Framework Core 4.3.4");
+		list.add("Spring Framework WebMvc 4.3.4");
+		list.add("Spring Framework JDBC 4.3.4");
+		
+		response.put("items", list);
+		return response;
+	}
+	
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
+	public String html(){
+		return "index";
 	}
 	
 }
